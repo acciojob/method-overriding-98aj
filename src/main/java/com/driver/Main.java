ecf214ec5b896fbbd151a22b3088029877adc7e5
98aj <103376356+98aj@users.noreply.github.com>
2022-12-17 08:18:30 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class Main {
-  
+ static class A{
+        public static String met(){
+            String s = "Invoking method from class A";
+            return s;
+        }
+    }
+    static class B extends A{
+        public static String met(){
+            String s1 = "Method is overridden in Extendend class B";
+            return s1;
+        }
+    }
+    public static void main(String[] args) {
+        B st = new B();
+        System.out.println(st.met());
+    }
 }
